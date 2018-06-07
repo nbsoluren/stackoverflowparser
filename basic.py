845424a00d31d4b62aba0e144e0f582f87d1356b
nbsoluren <nbsoluren@up.edu.ph>
2018-06-07 13:17:21 +0800

    Added retrival of answers & identification if code

--- basic.py
+++ basic.py
@@ -15,3 +15,32 @@ title = soup.find('title')
 
 #Prints the Question of Stack Overflow
 print (title.renderContents())
+file = open("mydivs.txt","a")
+
+# p_tags = soup.find(class_="user-review").find_all('p')
+# for p in p_tags[1:]: #find all p tag and exclude the first one
+#     print(p.text)
+ansCnt = 0
+answercell = soup.findAll("div", {"class": "answercell"})
+for ans in answercell:
+    ansCnt+=1
+    file.write("Answer #: " + str(ansCnt) + '\n')
+    textblock = ans.findAll("div", {"class": "post-text"})
+    for p in textblock:
+        p = ans.findAll(['p','code'])
+        for fin in p:
+            if(fin.name == 'code'):
+                file.write ("code:"+'\n')
+                file.write ('\t'+fin.text+'\n')
+            else:
+                file.write (fin.text+'\n')
+
+
+mydivs = soup.findAll("div", {"class": "answercell"})
+# print(mydivs)
+
+
+
+#
+# file.write(str(mydivs))
+file.close()
