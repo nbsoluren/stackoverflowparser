4c586b28a3c7af0b0379132573f538c5f35fff73
nbsoluren <nbsoluren@up.edu.ph>
2018-06-08 12:32:23 +0800

    Added Comments, Modularized functions

--- basic.py
+++ basic.py
@@ -1,74 +1,66 @@
 from bs4 import BeautifulSoup
 import requests
+import json
+def main(url):
 
-#asks for user input
-#url = input("Enter Stack Overflow URL: ")
+    #initialization
+    r = requests.get(url)
+    data = r.text
+    soup = BeautifulSoup(data,"html.parser")
+    #Putting it all together
+    data = {}
+    data["question"] = stackParser(soup,"question")[0]
+    data["answers"] = stackParser(soup, "answers")
+
+    print(json.dumps(data))
+
+
+
+
+def stackParser(soup,type):
+
+    key = "answercell"
+    #retrieves header if question
+    if (type == "question"):
+        question = soup.find('a', {"class": "question-hyperlink"}).text
+        key = "postcell"
+        print("this is idenfied as a # QUESTION: ")
+
+    dataPack = []
+    for chunk in soup.findAll("div", {"class": key}):
+        data = {}
+        authors = []
+        comments = []
+        description = ""
+        upvotes = ""
+        for p in chunk.findAll("div", {"class": "post-text"}):
+            for fin in chunk.findAll(['p','code']):
+                if(fin.name == 'code'):
+                    description += ('\t'+fin.text+'\n')
+                else:
+                    description += (fin.text+'\n')
+
+        for p in chunk.findAll("div", {"class": "user-details"}):
+            for fin in p.findAll(['a']):
+                authors.append(fin.text)
+
+        for fin in chunk.find_next_sibling().findAll("li", {"class": "comment"}):
+                pseudocomments = {}
+                pseudocomments["score"] = fin.find("div", {"class": "comment-score"}).text
+                pseudocomments["comment"] = fin.find("span", {"class": "comment-copy"}).text
+                pseudocomments["author"] = fin.find("a", {"class": "comment-user"}).text
+                comments.append(pseudocomments)
+
+        upvotesans = chunk.find_previous_sibling().find("span", {"class": "vote-count-post"}).text
+
+        data["description"] = description
+        data["authors"] = authors
+        data["upvotes"] = upvotes
+        data["comments"] = comments
+        dataPack.append(data)
+    return dataPack
+
+
+
 url = 'https://stackoverflow.com/questions/645312/what-is-the-quickest-way-to-http-get-in-python'
-
-#initialization
-r  = requests.get(url)
-data = r.text
-soup = BeautifulSoup(data,"html.parser")
-
-#Retrieves the question
-question = soup.find('a', {"class": "question-hyperlink"}).text
-
-#Retrieve question Description
-description = ""
-postAuthors = []
-for desc in soup.findAll("div", {"class": "postcell"}):
-    for p in desc.findAll("div", {"class": "post-text"}):
-        for fin in desc.findAll(['p','code']):
-            if(fin.name == 'code'):
-                description += '\t'+fin.text+'\n'
-            else:
-                description += fin.text+'\n'
-    for p in desc.findAll("div", {"class": "user-details"}):
-        for fin in p.findAll(['a']):
-            postAuthors.append(fin.text)
-
-
-
-#Retrieves # of upvotes
-upvotes = soup.find("span", {"class": "vote-count-post"}).text
-
-#Retrieves answers of the question
-answers =[]
-for ans in soup.findAll("div", {"class": "answercell"}):
-    pseudoans = {}
-    ansAuthors = []
-    comments = []
-    anstext = ""
-    upvotesans = ""
-    for p in ans.findAll("div", {"class": "post-text"}):
-        for fin in ans.findAll(['p','code']):
-            if(fin.name == 'code'):
-                anstext += ('\t'+fin.text+'\n')
-            else:
-                anstext += (fin.text+'\n')
-    for p in ans.findAll("div", {"class": "user-details"}):
-        for fin in p.findAll(['a']):
-            ansAuthors.append(fin.text)
-
-
-    upvotesans = ans.find_previous_sibling().find("span", {"class": "vote-count-post"}).text
-
-
-    pseudoans["answer"] = anstext
-    pseudoans["authors"] = ansAuthors
-    pseudoans["upvotes"] = upvotesans
-    answers.append(pseudoans)
-
-
-#Putting it all together
-data = {}
-data["question"] = {
-    "header": question,
-    "description": description,
-    "authors": postAuthors,
-    "upvotes": upvotes,
-    "comments": comments
-}
-data["answers"] = answers
-
-print(data)
+main(url)
